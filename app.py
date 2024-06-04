8a0f08e0482b1a4c2b1ffbb33def0a9e9efa5daa
qingw <qingw0907@gmail.com>
2024-06-04 21:34:30 +0800

    add index page

--- app.py
+++ app.py
@@ -1,14 +1,28 @@
 
 from flask import Flask
 from flask import url_for
+from flask import render_template
 from markupsafe import escape
+
+name = "Qingw"
+movies = [
+        {'title': 'My neighbor Totoro', 'year': '1988'},
+        {'title': 'Dead Poets Socity', 'year': '1989'},
+        {'title': 'A Perfect World', 'year': '1993'},
+        {'title': 'Leon', 'year': '1994'},
+        {'title': 'Mahjong', 'year': '1996'},
+        {'title': 'Swallowtail Butterfly', 'year': '1996'},
+        {'title': 'King of Comedy', 'year': '1999'},
+        {'title': 'Devils on the Doorstep', 'year': '1999'},
+        {'title': 'Wall-E', 'year': '2000'},
+        {'title': 'The Pork of Music', 'year': '2012'}
+]
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return '<h1>Hello Totoro!</h1><img src="http://helloflask.com/totoro.gif">'
-
+    return render_template('index.html', name=name, movies=movies)
 @app.route('/user/<name>')
 def user_page(name):
     return f'User:{escape(name)}'
