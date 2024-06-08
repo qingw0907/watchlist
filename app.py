cfeb02e1be3c800e4ec1847c6e8a98366998b145
qingw <qingw0907@gmail.com>
2024-06-08 21:35:14 +0800

    create, edit and delete item by form

--- app.py
+++ app.py
@@ -5,9 +5,10 @@ from flask_sqlalchemy import SQLAlchemy
 from markupsafe import escape
 import os
 import click
-
+from flask import request, redirect, flash
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////' + os.path.join(app.root_path,'data.db')
+app.config['SECRET_KEY'] = 'dev'
 
 db = SQLAlchemy(app)
 
@@ -65,11 +66,45 @@ def forge():
 	click.echo('Done')
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
+	if request.method == 'POST':
+		title = request.form.get('title')
+		year = request.form.get('year')
+		if not title or not year or len(year) > 4 or len(title) > 60:
+			flash('Invalid input.')
+			return redirect(url_for('index'))
+		movie = Movie(title=title, year=year)
+		db.session.add(movie)
+		db.session.commit()
+		flash('Item created.')
+		return redirect(url_for('index'))	
 	movies = Movie.query.all()
 	return render_template('index.html', movies=movies)
 
+@app.route('/movie/edit/<int:movie_id>', methods=['POST', 'GET'])
+def edit(movie_id):
+	movie = Movie.query.get_or_404(movie_id)
+	if request.method == 'POST':
+		title = request.form['title']
+		year = request.form['year']
+		if not title or not year or len(title) > 60 or len(year) > 4:
+			flash('Invalid input.')
+			redirect(url_for('edit', movie_id=movie_id))
+		movie.year = year
+		movie.title = title
+		db.session.commit()
+		flash('Item updated.')
+		return redirect(url_for('index'))
+
+	return render_template('edit.html', movie=movie)
+@app.route('/movie/delete/<int:movie_id>', methods=['POST'])
+def delete(movie_id):
+	movie = Movie.query.get_or_404(movie_id)
+	db.session.delete(movie)
+	db.session.commit()
+	flash("Item deleted")
+	return redirect(url_for('index'))
 @app.route('/user/<name>')
 def user_page(name):
     return f'User:{escape(name)}'
